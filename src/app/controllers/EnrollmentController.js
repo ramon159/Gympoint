16cb4bfebfbbd6215e6b349bfefe4d690804c839
Ramon Arthur <ramonarthur14@gmail.com>
2021-05-31 19:39:17 -0300

    enrollmentController: function index e uptade done

--- src/app/controllers/EnrollmentController.js
+++ src/app/controllers/EnrollmentController.js
@@ -1,9 +1,30 @@
+/* eslint-disable camelcase */
 import { addMonths, parseISO, isBefore } from 'date-fns';
 import { Enrollment, Plan, Student } from '../models';
 
 export default {
   async index(req, res) {
-    const enrollments = await Enrollment.findAll();
+    const { page = 1 } = req.query;
+
+    const enrollments = await Enrollment.findAll({
+      attributes: ['id', 'price', 'start_date', 'end_date'],
+
+      order: ['id'],
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'price'],
+        },
+      ],
+    });
+
     return res.json(enrollments);
   },
 
@@ -19,10 +40,6 @@ export default {
         .json({ message: "past dates/hours isn't permitted " });
     }
 
-    const plan = await Plan.findOne({
-      where: { id: plan_id },
-      attributes: ['price', 'duration'],
-    });
     // check if student that in use
     const enrolledStudent = await Enrollment.findOne({
       where: { student_id },
@@ -30,16 +47,22 @@ export default {
     if (enrolledStudent) {
       return res.status(404).json({ message: 'student already enrolled' });
     }
+
     // check student_id
     const student = await Student.findByPk(student_id);
+
+    if (!student) {
+      return res.status(404).json({ message: 'student id not found' });
+    }
+
+    const plan = await Plan.findOne({
+      where: { id: plan_id },
+      attributes: ['price', 'duration'],
+    });
 
     // check plan_id
     if (!plan) {
       return res.status(404).json({ message: 'plan id not found' });
-    }
-
-    if (!student) {
-      return res.status(404).json({ message: 'student id not found' });
     }
 
     const price = plan.price * plan.duration;
@@ -59,7 +82,67 @@ export default {
 
   async update(req, res) {
     const { id } = req.params;
-    return res.json(id);
+    const { student_id, plan_id, start_date } = req.body;
+
+    // transform string in Date()
+    const formattedStartDate = parseISO(start_date);
+
+    const enrollment = await Enrollment.findByPk(id);
+
+    if (start_date !== enrollment.start_date) {
+      if (isBefore(formattedStartDate, new Date())) {
+        return res
+          .status(404)
+          .json({ message: "past dates/hours isn't permitted " });
+      }
+    }
+
+    // check if student alredy enrolled
+
+    if (student_id) {
+      const enrolledStudent = await Enrollment.findOne({
+        where: { student_id },
+      });
+
+      // can exclude in where?
+
+      if (
+        enrolledStudent &&
+        enrolledStudent.student_id !== enrollment.student_id
+      ) {
+        return res.status(404).json({ message: 'student already enrolled' });
+      }
+
+      const studentExists = await Student.findByPk(student_id);
+
+      if (!studentExists) {
+        return res.status(404).json({ message: 'student id not found' });
+      }
+    }
+
+    // check if student exist
+    if (plan_id) {
+      if (!start_date) {
+        return res.status(404).json({ message: 'start_date id not found' });
+      }
+      const plan = await Plan.findOne({
+        where: { id: plan_id },
+        attributes: ['price', 'duration'],
+      });
+
+      // check plan_id
+      if (!plan) {
+        return res.status(404).json({ message: 'plan id not found' });
+      }
+
+      req.body.price = plan.price * plan.duration;
+
+      // eslint-disable-next-line camelcase
+      req.body.end_date = addMonths(formattedStartDate, plan.duration);
+    }
+    const newEnrollment = await enrollment.update(req.body);
+
+    return res.json(newEnrollment);
   },
 
   async delete(req, res) {
